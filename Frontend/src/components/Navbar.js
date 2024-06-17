06757c732a2ddfacb53c5196c227d121a2b0d044
Sai Rathan <maheshwaramrathan@gmail.com>
2024-06-17 09:45:11 +0530

    updated categories page

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Navbar.css';
-import logo from 'images/logo.png'
+import logo from 'images/logo.png';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <img src={logo} alt="Event Ticketing Logo" className="navbar-logo" />
-        Event Ticketing
+        <a href="/" className="navbar-brand-link">
+          <img src={logo} alt="Event Ticketing Logo" className="navbar-logo" />
+          Event Ticketing
+        </a>
       </div>
       <ul className="navbar-links">
         <li><a href="/events">Events</a></li>
