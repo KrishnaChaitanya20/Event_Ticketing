841183013d2e379cf44c48a466710fc02b71c20d
D Krishna Chaitanya <krishnachaitanyadevunoori@gmail.com>
2024-06-19 15:17:55 +0530

    Add login context

--- Frontend/src/pages/Login/Loginbody.js
+++ Frontend/src/pages/Login/Loginbody.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useLogin } from 'LoginContext';
 import './Loginbody.css';
 
 const Login = () => {
+  const {user,setUser}=useLogin();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('/login', {
+    const response = await fetch(process.env.REACT_APP_API_BASE_URL+'/users/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
     });
 
     const data = await response.json();
-    alert(data.message);
-  };
+    console.log(data);
+    if (data.error) {
+      alert(data.error);
+    } else {
+      setUser(data);
+    }
+  }
 
   return (
     <div className="container">
@@ -47,7 +54,7 @@ const Login = () => {
           <button type="submit">Login</button>
         </form>
         <div className="link">
-          New user? <Link to="/signup">Signup</Link>
+          New user? <Link to="/signup"> Signup</Link>
         </div>
       </div>
     </div>
