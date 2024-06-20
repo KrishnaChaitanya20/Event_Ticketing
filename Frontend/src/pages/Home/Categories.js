fb2debede5fe351ea83701b7189546d5f2acd1aa
Nishikanth2023 <b21cs019@kitsw.ac.in>
2024-06-20 15:43:25 +0530

    Added images to homepage

--- Frontend/src/pages/Home/Categories.js
+++ Frontend/src/pages/Home/Categories.js
@@ -2,14 +2,15 @@
 
 import React from 'react';
 import './Categories.css';
+import images from 'util/category';
 
 const categories = [
-  { id: 1, name: 'Music' },
-  { id: 2, name: 'Sports' },
-  { id: 3, name: 'Art' },
-  { id: 4, name: 'Technology' },
-  { id: 5, name: 'Food & Drink' },
-  { id: 6, name: 'Health' },
+  { id: 1, name: 'Concert' , image: images.concert },
+  { id: 2, name: 'Sports' , image: images.sport},
+  { id: 3, name: 'Art' , image: images.arts },
+  { id: 4, name: 'Technology' , image: images.tech},
+  { id: 5, name: 'Food & Drink' , image: images.food},
+  { id: 6, name: 'Health', image: images.health},
 ];
 
 const EventsByCategories = () => {
@@ -19,9 +20,9 @@ const EventsByCategories = () => {
         <h2>Events by Categories</h2>
       </div>
       <div className="categories-container">
-        {categories.map((category) => (
-          <div key={category.id} className="card">
-            {category.name}
+        {categories.map((category, index) => (
+          <div key={index} className="card" style={{ backgroundImage: `url(${category.image})` }}>
+            {category.name}  
           </div>
         ))}
       </div>
